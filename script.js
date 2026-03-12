7bbf89756428391abef3a1b00cf4c994f9c7245c
daniilsuhanov2010-debug <daniil.suhanov.2010@gmail.com>
2026-03-12 10:57:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+const ctx = document.getElementById("skillsChart");
 
+new Chart(ctx, {
+
+    type: "bar",
+
+    data: {
+
+        labels: ["HTML", "CSS", "JavaScript", "Teamwork", "Communication"],
+
+        datasets: [{
+
+            label: "Skill Level",
+
+            data: [70, 60, 50, 90, 85],
+
+            borderWidth: 1
+
+        }]
+
+    },
+
+    options: {
+
+        scales: {
+
+            y: {
+
+                beginAtZero: true,
+                max: 100
+
+            }
+
+        }
+
+    }
+
+});
